d8e631a687fd75842f2a33c25bda3f358bbb9f19
Konstantin Babeshin <connect@babeshin.space>
2023-03-03 09:37:54 +0500

    Добавлен слушаетель событий для календаря

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Events;
+use App\Observers\EventObs;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -25,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Events::observe(EventObs::class);
     }
 
     /**
