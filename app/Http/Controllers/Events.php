3f87e749037f06c4b068350a1b924402b718a033
orlovaPolina <pus120992@gmail.com>
2023-03-18 01:53:21 +0500

    Добавил метод start

--- app/Http/Controllers/Events.php
+++ app/Http/Controllers/Events.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SebastianBergmann\Type\VoidType;
 
 class Events extends Controller
 {
-    //
+    public function start() : void{
+        
+    }
 }
