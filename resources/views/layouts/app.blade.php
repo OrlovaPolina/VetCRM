f4af0737910f802306372556824c432df610a0cc
orlovaPolina <pus120992@gmail.com>
2023-03-24 00:24:59 +0500

    Добавлен bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+        @vite(['resources/css/app.css', 'resources/js/app.js','resources/js/app.js', 'resources/css/app.scss',
+            'resources/js/calendar.js'])
         <script src="js/fullCalendar/index.global.js"></script>
-        <script src="js/calendar.js"></script>
     </head>
     <body class="font-sans antialiased">  
         <style>
