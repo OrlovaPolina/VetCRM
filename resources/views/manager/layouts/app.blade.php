4807e1b90426d783bf97dcb415274de5efaff57a
orlovaPolina <pus120992@gmail.com>
2023-03-24 01:13:30 +0500

    Добалены стили для таблицы с пользователями

--- resources/views/manager/layouts/app.blade.php
+++ resources/views/manager/layouts/app.blade.php
@@ -12,7 +12,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js','resources/js/app.js', 'resources/css/app.scss',
+        @vite([
+            'resources/css/app.css',
+            'resources/css/manager.scss',
+            'resources/js/app.js',
+            'resources/js/app.js',
+            'resources/css/app.scss',
            ])
     </head>
     <body class="font-sans antialiased">  
