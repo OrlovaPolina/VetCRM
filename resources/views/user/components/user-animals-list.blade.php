6d282a6117aa2dca278cfa07c6a8f4f8876e3c74
Konstantin Babeshin <connect@babeshin.space>
2023-04-25 06:18:13 +0500

    Добавен функционал пользователя

---
+++ resources/views/user/components/user-animals-list.blade.php
@@ -0,0 +1,64 @@
+<div class="animal-box container-fluid d-flex position-relative w-100 p-10">    
+    @if(isset($animals))
+    @if(count($animals)>=0)
+    @foreach ($animals as $animal)
+        <div class="animal container-fluid d-flex position-relative w-90 start-50 translate-middle-x">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Кличка</th>
+                    </tr>
+                    <tr>
+                        <th>Порода</th>
+                    </tr>
+                    <tr>
+                        <th>Вид</th>
+                    </tr>
+                    <tr>
+                        <th>Дата последнего посещения</th>
+                    </tr>
+                    <tr>
+                        <th>Вес</th>
+                    </tr>
+                    <tr>
+                        <th>Возраст</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{$animal->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>{{$animal->breed->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>{{$animal->species->name}}</td>
+                    </tr>
+                    <tr>
+                        @php
+                        if($events[$animal->id]){
+                            $date = new DateTimeImmutable($events[$animal->id]);
+                            $date = $date->format('Y-m-d H:i');
+                        }
+                        @endphp
+                        <td>{{$events[$animal->id] ? $date  : '-'}}</td>
+                    </tr>
+                    <tr>
+                        <td>{{$visits[$animal->id]  ? $visits[$animal->id] .' кг': '-'}}</td>
+                    </tr>
+                    <tr>
+                        <td>{{$animal->age}}</td>
+                    </tr>
+                    <tr>
+                        <td><a class="download" href="{{route('user.download')}}">Карточка животного</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    @endforeach
+    @endif
+    @endif
+</div>
--- resources/views/user/layouts/animals.blade.php
+++ resources/views/user/layouts/animals.blade.php
@@ -1,5 +1,9 @@
 @include('layouts.header')
 <main>
-    
+    <x-user-animals-list :animals="$animals" :events="$events" :visits="$visits"/>
+    <div class="btn-box btn-add d-flex justify-content-evenly position-relative top-50 start-50 translate-middle-x">
+        <i class="bi bi-plus-circle"></i>
+        <a href="{{route('user.animalsCreatePage')}}">Добавить животное</a>
+    </div>
 </main>
 @include('layouts.footer')
