ec0657a1e7a7cb4380d22346340989ead87ad07e
orlovaPolina <pus120992@gmail.com>
2023-06-01 01:53:31 +0500

    последние правки

--- routes/auth.php
+++ routes/auth.php
@@ -59,6 +59,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+                
     Route::name('manager.')->group(function () {
         /**
          * Личный кабинет менеджера с пользователями
